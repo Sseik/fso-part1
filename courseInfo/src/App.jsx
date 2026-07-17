96e7542b15cfd48ac3b91fd9e5a54a445b10818e
Dmytro Hapchyn <gapcindmitro@gmail.com>
2026-07-17 20:35:59 +0300

    Complete exercises 1.1 and 1.2

---
+++ courseInfo/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = ({ title }) => <h1>{title}</h1>;
+
+const Part = ({ part, exercises }) => (
+  <>
+    <p>
+      {part} {exercises}
+    </p>
+  </>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part part={part.part} exercises={part.exercises} />
+    ))}
+  </>
+);
+
+const Total = ({ exercises }) => (
+  <p>
+    Number of exercises:{" "}
+    {exercises.reduce((sum, exercise) => sum + exercise, 0)}
+  </p>
+);
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content
+        parts={[
+          { part: part1, exercises: exercises1 },
+          { part: part2, exercises: exercises2 },
+          { part: part3, exercises: exercises3 }
+        ]}
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  );
+};
+
+export default App;
